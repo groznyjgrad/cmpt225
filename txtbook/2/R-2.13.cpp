7b608b9574cad0e378dae3bb592f4cff5a4907fa
groznyjgrad <sz.apt-get@live.com>
2019-05-26 19:51:01 -0700

    another problem

--- txtbook/2/R-2.13.cpp
+++ txtbook/2/R-2.13.cpp
@@ -2,12 +2,13 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 class OutOfBounds
 {
 public:
 	OutOfBounds(string msg) {errorMsg = msg;}
-	get_errorMsg() {return errorMsg;}
+	string get_errorMsg() {return errorMsg;}
 private:
 	string errorMsg;
 };
@@ -15,10 +16,10 @@ private:
 int getElement(int arr[], int len, int i)
 {
 	try {
-		return arr[i];
 		if (i > len) {
 			throw OutOfBounds("Array index out of bounds!"); 
 		}
+		return arr[i];
 	}
 
 	catch(OutOfBounds& oobError) {
@@ -30,8 +31,15 @@ int getElement(int arr[], int len, int i)
 int main()
 {
 	int myArr[] = {1, 2, 3, 4};
-	cout << getElement(myArr[], 3) << endl; // valid index, returns 3
+	cout << getElement(myArr, 3, 3) << endl; // valid index, returns 3
 
-	cout << getElement(myArr[], 4) << endl; // invalid index
-
+	cout << getElement(myArr, 3, 4) << endl; // invalid index
 }
+
+/*
+Trying to use try-catch blocks. Program is a bit silly since you have to 
+input the array length already, so its clear when you are using an invalid
+index.
+
+
+*/
