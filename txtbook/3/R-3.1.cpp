45ce34236e532942e698ac95445a63acd96ad2e5
groznyjgrad <sz.apt-get@live.com>
2019-06-04 23:38:40 -0700

    assign 1

---
+++ txtbook/3/R-3.1.cpp
@@ -0,0 +1,13 @@
+// Modify implementation of Scores so that at most maxEnt/2 of the scores can come from
+// a single player. 
+
+#include <iostream>
+#include <string>
+#include "R-3.1.h"
+
+// GameEntry members
+
+GameEntry::GameEntry(const string& )
+{
+
+}
