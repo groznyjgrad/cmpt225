3844e60a9e22fa047556b778cc641623e27618e6
groznyjgrad <sz.apt-get@live.com>
2019-06-05 23:46:59 -0700

    update assign 1

--- assign/charList.cpp
+++ assign/charList.cpp
@@ -5,9 +5,11 @@
 // Assignment 1: Due June 12 2019
 
 #include "charList.h"
+
 using namespace std;
 
 // CharDLL methods
+
 CharDLL::CharDLL() {
 	element_ = NULL;
 	prev_ = NULL;
@@ -18,20 +20,79 @@ CharDLL::~CharDLL() {}
 
 // CharList methods
 
-bool CharList::empty() const {				// returns 1 if empty, 0 if not empty
-	return head_->next_ == tail_;
+bool CharList::Empty() const {        // returns 1 if empty, 0 if not empty
+	return (head_->next_ == tail_);
 }
 
-void CharList::append(char c) {
- 	CharDLL* new_char = new CharDLL;
- 	new_char->element_ = c;
+void CharList::Append(char c) {
+	CharDLL* new_char = new CharDLL;
+	new_char->element_ = c;
 
-	CharDLL* old_last = tail_->prev_;		// get pointer to the current last node
-	tail_->prev_ = new_char;				// set last node to newly-appended node
-	old_last->next_ = new_char;				// set the old last node to point to the new last node
+	CharDLL* old_last = tail_->prev_;   // get pointer to the current last node
+	tail_->prev_ = new_char;        		// set last node to newly-appended node
+	old_last->next_ = new_char;      	  // set the old last node to point to the new last node
 
-	new_char->prev_ = old_last;				
+	new_char->prev_ = old_last;       
 	new_char->next_ = tail_;
+}
+
+void CharList::Append(char c, char d) {
+	CharDLL* new_char = new CharDLL;
+	new_char->element_ = c;
+	CharDLL* current_node = head_->next_;
+
+	while (current_node != tail_ && current_node->element_ != d) {
+		current_node = current_node->next_;
+	}
+	// current_node is positioned at first occurrence of 'd',
+	// or tail if 'd' did not occur
+
+	if (current_node == tail_) {
+		tail_->prev_->next_ = new_char; 	// connect rest of list to new node first
+		new_char->prev_ = tail_->prev_;
+
+		new_char->next_ = tail_;
+		tail_->prev_ = new_char;
+		return;
+	}
+
+	// current_node is at 'd'
+	current_node->next_->prev_ = new_char;  // nb: this is undefined if current_node == tail_
+	new_char->next_ = current_node->next_;  
+
+	new_char->prev_ = current_node;
+	current_node->next_ = new_char;
+}
+
+
+void CharList::Append(char c, char d, int n) {
+	CharDLL* new_char = new CharDLL;
+	new_char->element_ = c;
+	CharDLL* current_node = head_->next_;
+	int count = 0;
+
+	while (current_node != tail_ && count < n) {
+		if (current_node->element_ == d) {
+			count++;
+			if (count == n) {break;}
+		}
+		current_node = current_node->next_;
+	}
+
+	if (current_node == tail_) {
+		tail_->prev_->next_ = new_char; 
+		new_char->prev_ = tail_->prev_;
+
+		new_char->next_ = tail_;
+		tail_->prev_ = new_char;
+		return;
+	}
+
+	current_node->next_->prev_ = new_char;  
+	new_char->next_ = current_node->next_;  
+
+	new_char->prev_ = current_node;
+	current_node->next_ = new_char;
 }
 
 CharList::CharList(string str) {
@@ -40,29 +101,26 @@ CharList::CharList(string str) {
 	head_->next_ = tail_;
 	tail_->prev_ = head_;
 
-	for (int i = 0; i < str.length(); ++i)
-	{
-		append(str.at(i));			    	// construct CharList containing 's' through repeated appends
-	}
+	for (int i = 0; i < str.length(); ++i) { Append(str.at(i)); } 
 }
 
 CharList::~CharList() {
-	while (!empty())
+	while (!Empty())
 	{
-		CharDLL* remove_node = head_->next_;	// unlink the first node and delete it
-		head_->next_ = remove_node->next_;	// link the subsequent chain of nodes back to the head sentinel
+		CharDLL* remove_node = head_->next_;  // unlink the first node 
+		head_->next_ = remove_node->next_;    // link the rest of the list back to the head sentinel
 		delete remove_node;
 	}
-	// now all nodes are deleted, so delete sentinel nodes
+	// now all internal nodes are deleted, so delete sentinel nodes
 	delete head_;
 	delete tail_;
 }
 
-string CharList::to_string() {
+string CharList::ToString() {
 	string list_as_string;
 	CharDLL* current_node = head_->next_;
 
-	while (current_node != tail_)			// traverse list and append nodes
+	while (current_node != tail_)     // traverse list nodes and build string
 	{
 		list_as_string.push_back(current_node->element_);
 		current_node = current_node->next_;
@@ -70,41 +128,61 @@ string CharList::to_string() {
 	return list_as_string;
 }
 
-void CharList::insert(char c) {
- 	CharDLL* new_char = new CharDLL;
- 	new_char->element_ = c;
+void CharList::Insert(char c) {
+	CharDLL* new_char = new CharDLL;
+	new_char->element_ = c;
 
-	CharDLL* old_first = head_->next_;		// get pointer to the current first node
-	head_->next_ = new_char;				// set first node to newly-inserted node
-	old_first->prev_ = new_char;			// set the old first node to point to the new first node
+	CharDLL* old_first = head_->next_;    // get pointer to the current first node
+	head_->next_ = new_char;      		 	  // set first node to newly-inserted node
+	old_first->prev_ = new_char;     		  // set the old first node to point to the new first node
 
-	new_char->prev_ = head_;				
+	new_char->prev_ = head_;        
 	new_char->next_ = old_first;
 }
 
-void CharList::insert(char c, char d) {
+void CharList::Insert(char c, char d) {
 	CharDLL* new_char = new CharDLL;
- 	new_char->element_ = c;
- 	CharDLL* current_node = head_->next_;
+	new_char->element_ = c;
+	CharDLL* current_node = head_->next_;
 
- 	while (current_node != tail_ && current_node->element_ != d)
- 	{
- 		current_node = current_node->next_;
- 	}
- 	// now current node is positioned AT first occurrence of 'd', or tail if 'd' does not occur
+	while (current_node != tail_ && current_node->element_ != d)
+	{
+		current_node = current_node->next_;
+	}
+	// current_node is positioned at first occurrence of 'd',
+	// or tail if 'd' did not occur
 
- 	current_node->prev_->next_ = new_char;
- 	new_char->prev_ = current_node->prev_;
-
- 	current_node->prev_ = new_char;
- 	new_char->next_ = current_node;
+	current_node->prev_->next_ = new_char;
+	new_char->prev_ = current_node->prev_;
+	
+	current_node->prev_ = new_char;
+	new_char->next_ = current_node;
 }
 
-void CharList::insert(char c, char d, int n) {
+void CharList::Insert(char c, char d, int n) {
+	CharDLL* new_char = new CharDLL;
+	new_char->element_ = c;
+	CharDLL* current_node = head_->next_;
+	int count = 0;
 
+	while (current_node != tail_)
+	{
+		if (current_node->element_ == d)
+		{
+			count++;
+			if (count == n) {break;}
+		}
+		current_node = current_node->next_;
+	}
+
+	current_node->prev_->next_ = new_char;
+	new_char->prev_ = current_node->prev_;
+
+	current_node->prev_ = new_char;
+	new_char->next_ = current_node;
 }
 
-void CharList::remove(char c) {
+void CharList::Remove(char c) {
 	CharDLL* current_node = head_->next_;
 	while (current_node != tail_)
 	{
@@ -113,13 +191,13 @@ void CharList::remove(char c) {
 			current_node->prev_->next_ = current_node->next_;
 			current_node->next_->prev_ = current_node->prev_;
 			delete current_node;
-			return;							// immediate return means no subsequent 'c' will be removed
+			return;             // immediate return means no subsequent 'c' will be removed
 		}
-		current_node = current_node->next_; // traverse list while current node does not have 'c'
+		current_node = current_node->next_; 		// traverse list while current node does not have 'c'
 	}
 }
 
-void CharList::remove(char c, int n) {
+void CharList::Remove(char c, int n) {
 	CharDLL* current_node = head_->next_;
 	int count = 0;
 	while (current_node != tail_)
@@ -132,17 +210,32 @@ void CharList::remove(char c, int n) {
 				current_node->prev_->next_ = current_node->next_;
 				current_node->next_->prev_ = current_node->prev_;
 				delete current_node;
-				return;	
-			}					
+				return; 
+			}         
 		}
 		current_node = current_node->next_; 
 	}
 }
 
+int CharList::Size() const {
+	int count = 0;
+	CharDLL* current_node = head_->next_;
+	if (current_node == tail_) {return 0;}
+
+	while (current_node != tail_)
+	{
+		count++;
+		current_node = current_node->next_;
+	}
+	return count;
+}
 
 
-
-int CharList::size() const {}
-void CharList::append(char c, char d) {}
-void CharList::append(char c, char d, int n) {}
-
+// Notes
+// - Copied and pasted test code without changing cout variable name, this led to us thinking
+//   there was an error in the function, but there was not
+// - A number of segmentation faults during testing were due to misassignment of node pointers 
+//   by methods such that at runtime the program was trying to access methods of a NULL pointer
+// - Use str.at(i) to return the char at index i of string 'str'. Do not use str.data()
+// - There always exists a non-NULL current_node->prev_->next_ but this is not the case for
+//   current_node->next_->prev_; if current_node == tail_ then the latter is undefined (seg faults)
--- assign/charList.h
+++ assign/charList.h
@@ -5,44 +5,45 @@ using namespace std;
 class CharList;
 
 class CharDLL {
-friend class CharList;						// give private access to CharList
-public:
-	CharDLL();								// initializes a node without data
-	~CharDLL();								// destructor, frees all associated memory
+friend class CharList;										// give private access to CharList
+ public:
+	CharDLL();															// initializes a node without data
+	~CharDLL();															// destructor, frees all associated memory
 	
-private:	
-	char element_;							// character data held by node
-	CharDLL* prev_;							// pointer to previous node
-	CharDLL* next_;							// pointer to following node
+ private:	
+	char element_;													// character data held by node
+	CharDLL* prev_;													// pointer to previous node
+	CharDLL* next_;													// pointer to following node
 };
 
 
 class CharList {
-public:	
-	CharList(string s);						// constructor, initializes a list with contents of 's'
-	~CharList();							// destructor, frees all associated memory
+ public:	
+	CharList(string s);											// constructor, initializes a list with contents of 's'
+	~CharList();														// destructor, frees all associated memory
 
-	bool empty() const;						// is list empty?
-	int size() const;						// return number of elements in list
-	string to_string();						// return characters in list as a string
+	bool Empty() const;											// is list empty?
+	int Size() const;												// return number of elements in list
+	string ToString();											// return characters in list as a string
 
-	void insert(char c);					// inserts 'c' at the front of the list
-	void insert(char c, char d);			// inserts 'c' before first occurrence of 'd', 
-											// or EOL if 'd' does not occur
-	void insert(char c, char d, int n);     // inserts 'c' before the n-th occurrence of 'd',
-											// or EOL if 'd' does not occur exactly n times
+	void Insert(char c);										// inserts 'c' at the front of the list
+	void Insert(char c, char d);						// inserts 'c' before first occurrence of 'd', 
+																					// or EOL if 'd' does not occur
+	void Insert(char c, char d, int n);     // inserts 'c' before the n-th occurrence of 'd',
+																					// or EOL if 'd' does not occur at least n times
+																					// n must be > 0
 
-	void append(char c);					// inserts 'c' at the back of the list
-	void append(char c, char d);			// inserts 'c' after the first occurrence of 'd'
-											// or EOL if 'd' does not occur
-	void append(char c, char d, int n);		// inserts 'c' after the n-th occurrence of 'd',
-											// or EOL if 'd' does not occur exactly n times
+	void Append(char c);										// inserts 'c' at the back of the list
+	void Append(char c, char d);						// inserts 'c' after the first occurrence of 'd'
+																					// or EOL if 'd' does not occur
+	void Append(char c, char d, int n);			// inserts 'c' after the n-th occurrence of 'd',
+																					// or EOL if 'd' does not occur at least n times
 
-	void remove(char c);					// deletes the first occurrence of 'c',
-											// or nothing if 'c' does not occur
-	void remove(char c, int n);				// deletes the n-th occurrence of 'c',
-											// or nothing if 'c' does not occur exactly n times
-private:
-	CharDLL* head_;							// pointer to header node (sentinel)
-	CharDLL* tail_;							// pointer to trailer node (sentinel)
+	void Remove(char c);										// deletes the first occurrence of 'c',
+																					// or nothing if 'c' does not occur
+	void Remove(char c, int n);							// deletes the n-th occurrence of 'c',
+																					// or nothing if 'c' does not occur at least n times
+ private:
+	CharDLL* head_;													// pointer to header node (sentinel)
+	CharDLL* tail_;													// pointer to trailer node (sentinel)
 };
